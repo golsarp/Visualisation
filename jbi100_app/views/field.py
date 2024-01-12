172d26078258a76828824d445c25125e3cbf893f
golsarp <golsarp01@gmail.com>
2024-01-12 15:05:11 +0100

    Allignment

--- jbi100_app/views/field.py
+++ jbi100_app/views/field.py
@@ -8,6 +8,7 @@ from jbi100_app.config import (
 )
 
 import pandas as pd
+import time
 
 
 class Field(html.Div):
@@ -72,12 +73,15 @@ class Field(html.Div):
 
         return group["corrected_y"]
 
-    def positionPlayer(self, home, away, home_form, away_form):
+    def positionPlayer(self, home, away, home_form, away_form, selected_players):
         df_home = self.process_df(home, True)
         df_away = self.process_df(away, False)
         # Bench players also extracted
         df_home_field, df_home_bench = self.select_players(df_home, home_form)
         df_away_field, df_away_bench = self.select_players(df_away, away_form)
+
+        home_table = df_home_bench[["player", "position", "birth_year"]]
+        away_table = df_away_bench[["player", "position", "birth_year"]]
 
         max_val_home = df_home_field["position_y"].max()
         max_val_away = df_away_field["position_y"].max()
@@ -96,42 +100,98 @@ class Field(html.Div):
         )
 
         df_concat = pd.concat([df_home_field, df_away_field], ignore_index=True)
+        # df_concat["selected"] = df_concat["player"].isin(selected_players)
+        # Apply conditions directly to create the 'color' column
+        df_concat["color"] = df_concat.apply(
+            lambda row: "Yellow"
+            if row["player"] in selected_players
+            else "Blue"
+            if row["team"] == home
+            else "Red"
+            if row["team"] == away
+            else "Other",
+            axis=1,
+        )
 
+        # print(selected_players)
+        # print(df_concat.columns)
+        # print(df_concat)
+        # time.sleep(0.1)
+        # player age minutes_90s miscontrols
+        # print(px.colors.qualitative.Set1)
         hover_columns = [
             "player",
             # "position",
             # "team",
             "age",
-            "birth_year",
+            "minutes_90s",
+            "miscontrols"
             # "dispossessed",
             # "passes_received",
             # "progressive_passes_received",
         ]
-
+        color_mapping = {"Green": "green", "Blue": "blue", "Red": "red"}
         self.fig = px.scatter(
             df_concat,
             y="corrected_y",
-            # y="position_y",
             x="position_x",
-            color="position",
+            # color="team",
+            color="color",
             symbol="position",
             hover_data=hover_columns,
-            height=400,  # Set the height of the figure
+            height=370,  # Set the height of the figure
             width=900,
+            color_discrete_map=color_mapping,
         )
+        self.fig.update_layout(
+            # margin=dict(l=20, r=20, t=20, b=20),
+            margin=dict(l=20, r=20, t=10, b=0),
+            xaxis_title="",
+            yaxis_title="",
+            plot_bgcolor="rgb(0, 200, 0)",
+        )
+        self.fig.update_xaxes(showticklabels=False)
+        self.fig.update_yaxes(showticklabels=False)
+        self.fig.update_xaxes(showgrid=True, gridcolor="rgb(0, 128, 0)")
+        self.fig.update_yaxes(showgrid=True, gridcolor="rgb(0, 128, 0)")
+
+        # # Update hover template to exclude unwanted columns
+        # hover_template = "<br>".join(
+        #     [
+        #         "{}: %{{customdata[{}]}}".format(col, i)
+        #         for i, col in enumerate(hover_columns)
+        #     ]
+        # )
+
+        # self.fig.update_traces(
+        #     hovertemplate=hover_template,
+        #     customdata=df_concat[hover_columns].values.tolist(),
+        # )
 
         # Add annotations (text on top of values)
         for i, row in df_concat.iterrows():
+            player_name_parts = row["player"].split()  # Split the full name into parts
+            last_name = None
+            # Use only the first part of the name
+            if len(player_name_parts) > 1:
+                last_name = player_name_parts[1] if player_name_parts else ""
+            else:
+                last_name = player_name_parts[0]
             self.fig.add_annotation(
                 x=row["position_x"],  # x-coordinate of the annotation
                 y=row["corrected_y"],
                 # y=row["position_y"],  # y-coordinate of the annotation
                 # name=row["player"],
-                text=str(row["player"]),  # text to display
-                ax=0,
-                ay=-40,
+                # text=str(row["player"]),  # text to display
+                text=str(last_name),  # text to display
+                xshift=0,
+                yshift=+20,
+                showarrow=False,
+                # showarrow=False,
+                # ax=0,
+                # ay=-30,
             )
 
-        self.fig.update_traces(marker_size=10)
+        self.fig.update_traces(marker_size=15)
 
-        return self.fig, df_concat
+        return self.fig, df_concat, home_table, away_table
---
+++ jbi100_app/views/table.py
@@ -0,0 +1,69 @@
+from dash import dcc, html, dash_table
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import numpy as np
+
+
+class Table(html.Div):
+    def __init__(self, name):
+        self.html_id = name
+
+        # Equivalent to `html.Div([...])`
+        super().__init__(
+            # className="graph_card",
+            # children=[html.H6(name), dcc.Graph(id=self.html_id)],
+            children=[
+                dash_table.DataTable(
+                    id=self.html_id,
+                )
+            ],
+        )
+
+    def plot_table(self, df):
+        # print(df.columns)
+        # df["birth_year"] = 2023 - df["birth_year"]
+        # new_col_names = {
+        #      "Player",
+        #     "position": "Position",
+        #     "birth_year": "Age",
+        # }
+        new_column_names = ["Player", "Position", "Age"]
+        new_columns = [{"name": name, "id": name} for name in new_column_names]
+
+        # df = df.rename(columns=new_col_names)
+
+        # print(df)
+        return (
+            dash_table.DataTable(
+                id=self.html_id,
+                columns=[{"name": i, "id": i} for i in df.columns],
+                # columns=new_columns,
+                data=df.to_dict("records"),
+                style_cell=dict(textAlign="left"),
+                style_header=dict(backgroundColor="paleturquoise"),
+                style_data=dict(backgroundColor="lavender"),
+                style_data_conditional=[
+                    {
+                        "if": {"column_id": col, "column_editable": False},
+                        "backgroundColor": "rgba(255, 0, 0, 0.1)",
+                        "pointer-events": "none",  # Disable clicking on non-"player" columns
+                    }
+                    for col in df.columns
+                    if col != "player"
+                ]
+                + [
+                    {
+                        "if": {"column_id": "player", "state": "active"},
+                        "backgroundColor": "rgba(0, 116, 217, 0.3)",
+                    }
+                ],
+                virtualization=True,  # Enable virtualization for infinite scrolling
+                fixed_rows={"headers": True},  # Keep header fixed at the top
+                style_table={
+                    "height": "250px",
+                    # "width": "400px",
+                    "overflowY": "auto",
+                },  #
+            ),
+        )
